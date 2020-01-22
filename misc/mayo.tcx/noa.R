263bf05b7fff39e8a9c81ecb51d288e1b1749287
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-01-22 12:32:45 -0800

    ensure non-zero before log, that before scale, use spearmans always

--- misc/mayo.tcx/noa.R
+++ misc/mayo.tcx/noa.R
@@ -7,10 +7,28 @@ rownames(tbl.orig) <- ensg
 tbl.map <- select(EnsDb.Hsapiens.v79, key=ensg,  columns=c("SYMBOL"),  keytype="GENEID")
 dim(tbl.orig)
 dim(tbl.map)
+
+   #--------------------------------------------------------------------------------
+   # not all of the ensg ids have gene symbols.  remove those from the tbl.orig
+   #--------------------------------------------------------------------------------
+
 map.failures <- setdiff(rownames(tbl.orig), tbl.map$GENEID)
 map.failure.indices <- match(map.failures, rownames(tbl.orig))
 tbl <- tbl.orig[-map.failure.indices,]
 dim(tbl)
+dim(tbl.map)
+
+   #--------------------------------------------------------------------------------
+   # all the ensg row names now have geneSymbol names, but some of the lattter
+   # appear more than once.  rownames must be unique.  for instance, in tbl.map
+   #               GENEID     SYMBOL
+   # 2005 ENSG00000115239 GPR75-ASB3
+   # 2006 ENSG00000270898 GPR75-ASB3
+   # rows in tbl.map correspond to rows in tbl, so use which(duplicated(tbl.map$SYMBOL))
+   # to eliminate rows in tbl
+   # better yet: first sort on median or variance of expression of both genes
+   #--------------------------------------------------------------------------------
+
 dups <- which(duplicated(tbl.map$SYMBOL))
 length(dups)
 tbl <- tbl[-dups,]
@@ -42,24 +60,30 @@ if(minValue <= 0){
   mtx.from.noa <- mtx.from.noa + abs(minValue) + .Machine$double.eps
   }
 
-mtx.scaled <-  t(scale(t(mtx.from.noa)))
-plot(mtx.scaled[g1,], mtx.scaled[g2,])
-cor(mtx.scaled[g1,], mtx.scaled[g2,])   # 0.76
+boxplot(as.numeric(mtx.from.noa), main="mtx.from.noa")
+cor(mtx.from.noa[g1,], mtx.from.noa[g2,],method="spearman")   # 0.76  0.74
+quartz()
 
-mtx.log.scaled <- log10(mtx.scaled)
-plot(mtx.log.scaled[g1,], mtx.log.scaled[g2,])
+   #----------------------------------------------------------------------------
+   # max says the right approach is:  t(scale(t(mtx.log)))
+   #----------------------------------------------------------------------------
 
 mtx.log <- log10(mtx.from.noa)
-plot(mtx.log[g1,], mtx.log[g2,])
+quartz()
 
-mtx.scaled.log <- t(scale(t(mtx.log)))
+plot(mtx.from.noa[g1,], mtx.from.noa[g2,])   # 0.76
 plot(mtx.scaled.log[g1,], mtx.scaled.log[g2,])
 
-mtx.scaled <-  t(scale(t(mtx.2)))
-mtx.log.scaled <- t(scale(t(mtx.2)))
+plot(mtx.from.noa[g1,], mtx.log[g1,])   # 0.76
+plot(mtx.from.noa[g2,], mtx.log[g2,])   # 0.76
+boxplot(as.numeric(mtx.log), main="mtx.log")
+length(which(is.na(mtx.log)))
+cor(mtx.log[g1,], mtx.log[g2,], method="spearman" )
 
-cor(mtx.log.scaled[g1,], mtx.log.scaled[g2,])   # 0.407936
+mtx.scaled.log <- t(scale(t(mtx.log)))
+quartz()
+boxplot(as.numeric(mtx.scaled.log), main="mtx.scaled.log")
+length(which(is.na(mtx.scaled.log))) # 982,500
+cor(mtx.scaled.log[g1,], mtx.scaled.log[g2,], method="spearman")   # NA
 
-mtx.old <- get(load("~/github/TrenaProjectAD/inst/extdata/expression/temporalCortex.15167x264.RData"))
-cor(mtx.old[g1,], mtx.old[g2,])   # 0.8315445
 
